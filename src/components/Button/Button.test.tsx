88f8ff5a1b420c5b795fd36ef5f72786b7ea56ed
MichelleLinley <michelle.m.pitts@gmail.com>
2020-10-27 10:12:31 -0400

    button tests now working

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,16 +1,22 @@
+import React from 'react'
 import TestRenderer from 'react-test-renderer'
+import { Text } from 'react-native'
 import Button from './Button'
 
 describe('button', () => {
-    const testRenderer = TestRenderer.create(<Button type="primary" size="sm" text="Hello" onPress={()=>'pressed'} />);
-    const testInstance = testRenderer.root
-    console.log(testInstance)
-    it('should correctly pass props', () => {
-        console.log(testInstance)
-        expect(testInstance.findByType(Button).props.type).toBe("primary")
+    const renderTest = (type='accent', size= 'sm', text= "Sample Button", onPress=()=>'pressed') => (
+        TestRenderer.create(<Button type={type} size={size} text={text} onPress={onPress} />)
+    )
+
+    it('should correctly pass props', async () => {
+        const testRender = renderTest()
+        await expect(testRender.root.findByType(Button).props.type).toBe("accent")
+        testRender.unmount()
     })
 
-    // it('should contain a text element that matches the text prop', () => {
-    //     expect(testInstance.findByType(Text).props.children).toBe("Hello")
-    // })
+    it('should contain a text element that matches the text prop', async () => {
+        const testRender = renderTest()
+        expect(testRender.root.findByType(Text).props.children).toBe("Sample Button")
+        testRender.unmount()
+    })
 })
